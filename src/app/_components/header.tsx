d38b8c3f8dfb26019d2e52767ead40ea8065d6dc
Arsik0153 <arsik0153@gmail.com>
2024-12-27 23:55:31 +0900

    setup tailwind

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav className="">
-            <a href="#">Главная</a>
-            <a href="#">О нас</a>
-            <a href="#">Податься сейчас</a>
-            <a href="#">Задать вопрос</a>
+        <nav className="container mx-auto flex gap-6 py-10">
+            <a href="#" className="p-2.5 text-xl font-medium">
+                Главная
+            </a>
+            <a href="#" className="p-2.5 text-xl font-medium">
+                О нас
+            </a>
+            <a href="#" className="p-2.5 text-xl font-medium">
+                Податься сейчас
+            </a>
+            <a href="#" className="p-2.5 text-xl font-medium">
+                Задать вопрос
+            </a>
         </nav>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-'use client';
 import React from 'react';
-import FileIcon from '@/assets/icons/file.svg';
-import { useUser } from '@/components/providers/UserProvider';
 import Header from './_components/header';
 
 export default function Home() {
-    const { user } = useUser();
-
     return (
         <div>
             <Header />
